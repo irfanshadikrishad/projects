0c9777dd0adedaac070889e52aa3629cc067269d
irfanshadikrishad <irfanshadikofficial@gmail.com>
2023-01-22 03:18:15 +0600

    1 project

---
+++ src/Project.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Project(props) {
+  return (
+    <a href={props.link} className="project-box" target="_blank" rel="noreferrer">
+      <img className="project-img" src={props.img} alt="project thumbnail" />
+      <h4 className="project-title">{props.title}</h4>
+      <p className="project-desc">{props.desc}</p>
+      <h4 className="project-title">Collaborations:</h4>
+      <div class="collab-main">
+        <div class="collab">
+          <img src={props.collab1} alt="collaborators" class="collab-img" />
+        </div>
+
+        <div class="collab collab-2">
+          <img src={props.collab2} alt="collaborators" class="collab-img" />
+        </div>
+      </div>
+    </a>
+  );
+}
+
+export default Project;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Project from "./Project";
+import p1 from "./img/p-2.png";
+import collab1 from "./img/collab-1.jpg";
+import collab2 from "./img/collab-2.jpg";
 
 ReactDOM.render(
-  <div className="container">
-    <h1>Under Development</h1>
+  <div className="project">
+    <h1 className="title container">.project()</h1>
+    <div className="project-main container">
+      <Project
+        img={p1}
+        title="A Portfolio Website"
+        desc="A portfolio website for a ui/ux designer."
+        link="https://itsamuhaimin.github.io"
+        collab1={collab1}
+        collab2={collab2}
+      />
+    </div>
   </div>,
   document.getElementById("root")
 );
